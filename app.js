f7da52d2adddfbb14a5948f398088b5e2489dda2
jiyong <wlfyd0419@gmail.com>
2022-08-24 18:08:29 +0900

    #4.4

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const hello = document.querySelector("#hello");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function handleLoginFormSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  hello.classList.remove(HIDDEN_CLASSNAME);
+  hello.innerText = `Hello ${username}`;
   /*
   if (username === "") {
     alert("Please write your name");
