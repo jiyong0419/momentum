751d8ba4cd1cc4153edb4048bde6e3a3f365a55c
jiyong <wlfyd0419@gmail.com>
2022-08-24 17:04:27 +0900

    #4.1

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 function handleLoginBtnClick() {
-  console.log("hello" + loginInput.value);
+  const username = loginInput.value;
+  if (username === "") {
+    alert("Please write your name");
+  } else if (username.length > 15) {
+    alert("Your name is too long.");
+  }
 }
 
 loginButton.addEventListener("click", handleLoginBtnClick);
