ff7e936457e548243ea621c3628673e9446861d0
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-08-26 11:50:09 +0900

    #7.2

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,21 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function deleteTodo(e) {
+  const li = e.target.parentElement;
+  li.remove();
+  //   toDoList.removeChild(e.target.parentElement);
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const btn = document.createElement("button");
+  btn.innerText = "❌";
+  btn.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(btn);
   toDoList.appendChild(li);
 }
 
