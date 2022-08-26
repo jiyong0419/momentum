c5ff86c98db6b29984d2b751a3b539d4dfcaa5ce
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-08-26 13:35:40 +0900

    #7.3

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,21 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+const toDos = [];
+
+/**입력받은 Todo를 localStorage에 저장 */
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteTodo(e) {
   const li = e.target.parentElement;
   li.remove();
   //   toDoList.removeChild(e.target.parentElement);
 }
 
+/**Todo가 submit되었을때 Todo리스트에 li추가 */
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
@@ -25,7 +34,9 @@ function handleToDoSubmit(e) {
   e.preventDefault();
   const value = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(value);
   paintTodo(value);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
