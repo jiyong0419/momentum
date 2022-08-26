96648fbb932267ee14c4b2ddb8882729c2d2a7d4
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-08-26 20:34:00 +0900

    #8.1

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,18 @@
+const API_KEY = "c8f9dab946ccd5055b646b937d8daa7e";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = responseJson.name;
+      weather.innerText = responseJson.weather[0].main;
+    });
 }
 function onGeoError() {
   alert("Can't find you");
