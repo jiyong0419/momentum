62ef6b98b4d054848eb4c439223f5cf526ebc30e
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-08-26 11:22:53 +0900

    #7.1

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(e) {
   e.preventDefault();
   const value = toDoInput.value;
   toDoInput.value = "";
+  paintTodo(value);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
