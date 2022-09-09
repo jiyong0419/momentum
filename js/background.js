666568b343e44d154aad1ba64c0821403b28a652
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-09 22:06:04 +0900

    Final CSS4

--- js/background.js
+++ js/background.js
@@ -2,5 +2,5 @@
 const images = ["bg_image_1.jpg", "bg_image_2.avif", "bg_image_3.jpg", "bg_image_4.webp", "bg_image_5.png"];
 const randomImages = images[Math.floor(Math.random() * images.length)];
 const imageTag = document.createElement("img");
-imageTag.setAttribute("src", `/img/${randomImages}`);
+imageTag.src = `./img/${randomImages}`;
 document.body.appendChild(imageTag);
