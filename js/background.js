edda82ad9b8d4797c0d8796326a706334b9f6864
jiyong0419 <82662371+jiyong0419@users.noreply.github.com>
2022-09-09 21:40:50 +0900

    Final CSS2

--- js/background.js
+++ js/background.js
@@ -2,5 +2,5 @@
 const images = ["Christ_the_Redeemer.jpg", "Colosseum.avif", "Eiffel_Tower.jpg", "newyork.webp", "Taj_Mahal.png"];
 const randomImages = images[Math.floor(Math.random() * images.length)];
 const imageTag = document.createElement("img");
-imageTag.setAttribute("src", `img/${randomImages}`);
+imageTag.setAttribute("src", `/img/${randomImages}`);
 document.body.appendChild(imageTag);
